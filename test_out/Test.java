14bdc6d167a57493a65b1835b474db658bfaee6d
VladSG <vladislav.sg@yandex.ru>
2022-03-21 12:24:56 +0300

    delete unused files

--- test_out/Test.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Test {
-    public static void main(String[] args) throws IOException {
-        Calculator calculator = new Calculator("5+10/(2-4)*2");
-        Integer x = calculator.e().res;
-        System.out.println(x);
-    }
-}
